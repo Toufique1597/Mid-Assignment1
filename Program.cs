055e2f215cafc3ce02f00619144bc889ed4c3fb5
Toufique1597 <toufique1597@gmail.com>
2021-03-02 01:00:06 +0600

    update

--- Program.cs
+++ Program.cs
@@ -10,19 +10,32 @@ namespace Association_HAS_A_Relation__A_
     {
         static void Main(string[] args)
         {
-            //Address address1 = new Address(4,10,"Dhaka");
-            //Account account1 = new Account(1001, "Shakib", 2000, address1);
-            //account1.PrintAccount();
+            Address address1 = new Address(4,10,"Dhaka");
+            Account account1 = new Account( "Shakib", 2000, address1);
+            account1.ShowAccountInformation();
+            account1.Withdraw(500);
+            account1.Deposite(1000);
+
+            Address address2= new Address(9, 112, "Khulna");
+            Account account2= new Account("Tamim", 7000, address1);
+            account2.ShowAccountInformation();
+            account2.Withdraw(8000);
+            account2.Deposite(0);
 
             //Account account2 = new Account(1002,"Tamim",3000,new Address(7,20,"Chittagong"));
             //account2.PrintAccount();
 
-            Bank ourBank = new Bank("Developer's bank", 5);
-            ourBank.AddAccount(new Account(1001, "Shakib", 2000, new Address(4, 10, "Dhaka")));
-            ourBank.AddAccount(new Account(3003, "Mushfiq", 5000, new Address(4, 10, "Sylhet")));
-            ourBank.AddAccount(new Account(1002, "Tamim", 3000, new Address(7, 20, "Chittagong")));
-            //ourBank.PrintAllAccounts();
-            ourBank.SearchAccount(1001);
+          //  Bank ourBank = new Bank("Developer's bank", 5);
+           // ourBank.AddAccount(new Account( "Shakib", 2000, new Address(4, 10, "Dhaka")));
+         
+
+           // ourBank.AddAccount(new Account( "Mushfiq", 5000, new Address(4, 10, "Sylhet")));
+           // ourBank.AddAccount(new Account( "Tamim", 3000, new Address(7, 20, "Chittagong")));
+          //  ourBank.PrintAccountDetails();
+           // ourBank.Withdraw(new Account ());
+            //ourBank.SearchAccount(1001);
+           
+            
         }
     }
 }
